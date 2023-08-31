dfd6ef78711bb7e764fc2d67457f1c4b690733c9
kdanjana <103878831+kdanjana@users.noreply.github.com>
2023-08-31 11:02:02 +0530

    added * in allowed hosts

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y!ygnb%u@w4=e(fnoxn)7-v8x=&2y%uz@sys01i&iknj_u%w^i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
